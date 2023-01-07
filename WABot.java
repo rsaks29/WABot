cedf93f9b8a085c8ee8bd09bd8961f1302d372b7
goblin101 <57072351+goblin101@users.noreply.github.com>
2023-01-08 00:48:17 +0100

    Add files via upload

---
+++ WABot.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class WABot {
+    public static void main(String[] args) throws AWTException {
+        Robot bot = new Robot();
+//        bot.delay(500);
+//        System.out.println(MouseInfo.getPointerInfo().getLocation());
+        bot.mouseMove(73,856);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        bot.mouseMove(159,338);
+        bot.mousePress(InputEvent.BUTTON1_MASK);
+        bot.mouseRelease(InputEvent.BUTTON1_MASK);
+
+        for (int i = 0; i
+        sendKeys(bot, "Hello Mr. G");
+    }
+    public static void sendKeys(Robot bot, String whatToType) {
+        for (int i=0; i<whatToType.length(); i++) {
+            String letter = whatToType.substring(i, i+1);
+            char c=letter.charAt(0); // convert string into char
+            int keyCode=KeyEvent.getExtendedKeyCodeForChar(c);
+            bot.keyPress(keyCode);
+            bot.keyRelease(keyCode);
+        }
+        bot.keyPress(KeyEvent.VK_ENTER);
+        bot.keyRelease(KeyEvent.VK_ENTER);
+    }
+}
